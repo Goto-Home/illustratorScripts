2ffe9667ee56cbb5d7115f39bbf4234299f2aa0a
Goto-Home <165063631+Goto-Home@users.noreply.github.com>
2024-04-22 20:36:11 -0400

    Update CMYKcolorChart.js
    Now it functions as it is intended to and the text color (black) is commented out to work towards creating a Compound Shape, clear reveal where the text is on the square background

--- CMYKcolorChart.js
+++ CMYKcolorChart.js
@@ -4,8 +4,12 @@ var magenta = parseFloat(prompt("Enter Magenta value (0-100):"));
 var yellow = parseFloat(prompt("Enter Yellow value (0-100):"));
 var black = parseFloat(prompt("Enter Black value (0-100):"));
 
-// Create a new document
-var doc = app.documents.add();
+// Get the active document
+var doc = app.activeDocument;
+
+// Get the active artboard
+var artboard = doc.artboards[doc.artboards.getActiveArtboardIndex()];
+var artboardRect = artboard.artboardRect;
 
 // Set CMYK color
 var color = new CMYKColor();
@@ -14,11 +18,14 @@ color.magenta = magenta;
 color.yellow = yellow;
 color.black = black;
 
+// Set text color
+// var textColor = new CMYKColor();
+// textColor.black = 100; // Black
+
 // Calculate square position and size
 var squareSize = 144; // 2 inches = 144 points
-var artboard = doc.artboards[0];
-var centerX = artboard.artboardRect[2] / 2;
-var centerY = artboard.artboardRect[3] / 2;
+var centerX = (artboardRect[2] - artboardRect[0]) / 2 + artboardRect[0];
+var centerY = (artboardRect[3] - artboardRect[1]) / 2 + artboardRect[1];
 var left = centerX - squareSize / 2;
 var top = centerY + squareSize / 2;
 var right = left + squareSize;
@@ -27,13 +34,14 @@ var bottom = top - squareSize;
 // Create square
 var square = doc.pathItems.rectangle(top, left, squareSize, squareSize);
 square.fillColor = color;
+square.stroked = false;
 
 // Create text label
 var labelText = "CMYK (" + cyan + ", " + magenta + ", " + yellow + ", " + black + ")";
 var textX = centerX;
-var textY = centerY - squareSize / 2 + 5; // Position at the bottom, inside edge
+var textY = (centerY - squareSize / 2) + 14; // Position at the bottom, inside edge
 var textFrame = doc.textFrames.add();
 textFrame.contents = labelText;
-textFrame.textRange.characterAttributes.fillColor = textColor;
+textFrame.textRange.characterAttributes.fillColor = color;
 textFrame.position = [textX, textY];
 textFrame.textRange.paragraphAttributes.justification = Justification.CENTER;
