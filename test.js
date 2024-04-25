10cb6108217720b519ec1e8a334514791fe82cc7
Goto-Home <165063631+Goto-Home@users.noreply.github.com>
2024-04-24 22:07:40 -0400

    Update test.js

--- test.js
+++ test.js
@@ -3,24 +3,36 @@
 	var p; // panel
 	var g; // group
 	
-	w = new Window("dialog", "TITLE", undefined);
-	w.orientation = "column";
-
+	var w = new Window("dialog", "CMYK Palette", undefined);
+	
 	p = w.add("panel");
+	gInput = p.add("group");
 	g = p.add("group");
-	var btnOk = g.add("button", undefined, "OK");
+
+	// g.orientation = "column";
+	
+	gInput.add("statictext", undefined, "C:");// Add labels for input fields
+	var cInput = gInput.add("edittext", undefined, "0"); // Add input fields for CMYK values
+	cInput.preferredSize.width = 60; // Set width of the input box
+
+	gInput.add("statictext", undefined, "M:"); // Add labels for input fields
+    var mInput = gInput.add("edittext", undefined, "0"); // Add input fields for CMYK values
+	mInput.preferredSize.width = 60; // Set width of the input box
+
+	// Add Submit or Cancel buttons
+	var btnSubmit = g.add("button", undefined, "Submit");
 	var btnCancel = g.add("button", undefined, "Cancel");
 	
 	// UI EVENT HANDLERS
 	
-	btnOk.onClick = function() {
+	btnSubmit.onClick = function() {
 		w.close(1);
 	};
 	
 	btnCancel.onClick = function () {
 		w.close(0);
 	};
-	
+
 	// SHOW THE WINDOW
 	
 	// w.show();
