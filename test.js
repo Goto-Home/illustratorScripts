d4a2fa395f23b48cb586e9ba1273df312d0b1e21
Goto-Home <165063631+Goto-Home@users.noreply.github.com>
2024-04-25 12:27:39 -0400

    Update test.js

--- test.js
+++ test.js
@@ -12,12 +12,20 @@
 	// g.orientation = "column";
 	
 	gInput.add("statictext", undefined, "C:");// Add labels for input fields
-	var cInput = gInput.add("edittext", undefined, "0"); // Add input fields for CMYK values
-	cInput.preferredSize.width = 60; // Set width of the input box
+	var cyanInput = gInput.add("edittext", undefined, "0"); // Add input fields for CMYK values
+	cyanInput.preferredSize.width = 60; // Set width of the input box
 
 	gInput.add("statictext", undefined, "M:"); // Add labels for input fields
-    var mInput = gInput.add("edittext", undefined, "0"); // Add input fields for CMYK values
-	mInput.preferredSize.width = 60; // Set width of the input box
+    var magentaInput = gInput.add("edittext", undefined, "0"); // Add input fields for CMYK values
+	magentaInput.preferredSize.width = 60; // Set width of the input box
+	
+	gInput.add("statictext", undefined, "Y:"); // Add labels for input fields
+    var yellowInput = gInput.add("edittext", undefined, "0"); // Add input fields for CMYK values
+	yellowInput.preferredSize.width = 60; // Set width of the input box
+	
+	gInput.add("statictext", undefined, "K:"); // Add labels for input fields
+    var blackInput = gInput.add("edittext", undefined, "0"); // Add input fields for CMYK values
+	blackInput.preferredSize.width = 60; // Set width of the input box
 
 	// Add Submit or Cancel buttons
 	var btnSubmit = g.add("button", undefined, "Submit");
