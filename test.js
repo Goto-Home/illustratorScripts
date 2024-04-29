031082a42ae226cbdb4e97f07b00546c9a6dc396
Goto-Home <165063631+Goto-Home@users.noreply.github.com>
2024-04-28 22:58:39 -0400

    Update test.js

--- test.js
+++ test.js
@@ -8,9 +8,14 @@ var c = w.add("panel", undefined, undefined, {borderStyle: "none"});
 c.size = [200, 100]; // Set width to 200 and height to 100
 
 // Add a dummy element to control the panel's width
+var btnAlert = c.add("button", undefined, "Alert");
 var btnClose = c.add("button", undefined, "Close");
 
 // Define onClick event handler for the Close button
+btnAlert.onClick = function() {
+    alert("Hello, World!");
+}; 
+
 btnClose.onClick = function() {
     w.close(); // Close the window when the button is clicked
 }; 
